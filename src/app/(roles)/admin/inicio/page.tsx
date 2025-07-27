385930df04268f0a1016cf24f4f02eabcac40afb
CrishtianQL <cristianquijada870@gmail.com>
2025-07-26 22:05:30 -0400

    tracata

--- src/app/(roles)/admin/inicio/page.tsx
+++ src/app/(roles)/admin/inicio/page.tsx
@@ -7,13 +7,13 @@ import { Warehouse, Users } from 'lucide-react';
 export default function AdminInicioPage() {
   return (
     <motion.main
-      className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 text-white"
+      className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 text-black"
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.1, ease: 'easeOut' }}
     >
-      <h1 className="text-4xl font-bold tracking-tight mb-2">
-       <span className="text-blue-600">¡Bienvenido a</span> <span className="text-blue-600">Administrador!</span>!
+      <h1 className="text-2xl font-bold tracking-tight mb-2">
+       <span className="text-gray-700">Bienvenido a</span> <span className="text-gray-700">Administrador</span>
       </h1>
       <p className="text-gray-700 text-lg mb-6">
         Panel de gestión del sistema
@@ -23,31 +23,29 @@ export default function AdminInicioPage() {
       <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-10">
         {/* Usuarios registrados */}
         <motion.div
-          className="bg-gradient-to-br from-yellow-700 to-yellow-900 rounded-2xl p-6 shadow-md hover:shadow-xl transition"
-          whileHover={{ scale: 1.03 }}
-          transition={{ duration: 0.1 }}
+          className="bg-gradient-to-br from-white rounded-2xl p-6 shadow-md "
+          
         >
           <div className="flex items-center justify-between mb-4">
             <div>
               <h2 className="text-4xl font-bold">23</h2>
-              <p className="text-sm text-gray-300">Usuarios registrados</p>
+              <p className="text-sm text-black">Usuarios registrados</p>
             </div>
-            <Users className="w-10 h-10 text-yellow-100" />
+            <Users className="w-10 h-10 text-blue-700" />
           </div>
         </motion.div>
 
         {/* Almacenes activos */}
         <motion.div
-          className="bg-gradient-to-br from-gray-700 to-gray-900 rounded-2xl p-6 shadow-md hover:shadow-xl transition"
-          whileHover={{ scale: 1.03 }}
-          transition={{ duration: 0.1 }}
+          className="bg-gradient-to-br from-white to-white rounded-2xl p-6 shadow-md "
+        
         >
           <div className="flex items-center justify-between mb-4">
             <div>
               <h2 className="text-4xl font-bold">5</h2>
-              <p className="text-sm text-gray-300">Almacenes activos</p>
+              <p className="text-sm text-black">Almacenes activos</p>
             </div>
-            <Warehouse className="w-10 h-10 text-gray-100" />
+            <Warehouse className="w-10 h-10 text-green-600" />
           </div>
         </motion.div>
       </section>
@@ -56,13 +54,13 @@ export default function AdminInicioPage() {
       <section className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         <Link
           href="/admin/usuarios"
-          className="bg-yellow-700 hover:bg-yellow-800 text-white text-center font-semibold py-8 px-4 rounded-2xl transition text-lg shadow-md"
+          className="bg-white hover:bg-white text-black text-center font-semibold py-8 px-4 rounded-2xl transition text-lg shadow-md "
         >
           Gestionar usuarios
         </Link>
         <Link
           href="/admin/almacen"
-          className="bg-gray-800 hover:bg-gray-900 text-white text-center font-semibold py-8 px-4 rounded-2xl transition text-lg shadow-md"
+          className="bg-white hover:bg-white text-black text-center font-semibold py-8 px-4 rounded-2xl transition text-lg shadow-md"
         >
           Ver almacenes
         </Link>
