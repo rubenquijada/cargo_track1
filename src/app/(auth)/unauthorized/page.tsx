d57d2469c798af18d6b161ba577c019e274e29d8
Ruben Quijada <rubenquijada3@gmail.com>
2025-07-26 21:24:31 -0400

    no autorizado cambiado

--- src/app/(auth)/unauthorized/page.tsx
+++ src/app/(auth)/unauthorized/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function UnauthorizedPage() {
   return (
-    <div className="relative min-h-screen bg-black text-white flex items-center justify-center overflow-hidden">
+    <div className="relative min-h-screen bg-blue-100 text-white flex items-center justify-center overflow-hidden">
       {/* Este div ya centra su contenido horizontal y verticalmente */}
       <div className="relative z-20 max-w-sm w-full bg-transparent p-6 space-y-6 text-center">
-        <h2 className="text-2xl font-semibold text-white">Acceso denegado</h2>
+        <h2 className="text-2xl font-semibold text-gray-800">Acceso denegado</h2>
 
         <button
-          className="inline-block w-full rounded-md bg-gray-300 py-2 text-gray-700 font-semibold hover:bg-gray-500 transition"
+          className="inline-block w-60 rounded-md bg-black py-2 text-white font-semibold hover:bg-gray-400 transition"
           onClick={() => history.back()}
         >
           Volver atrás
